2221951adbd7da85b67e966033f6188981484d59
salvarezf <saaf@SAAF-MAC-PRO.local>
2018-08-24 21:21:40 +0200

    Added rest to requirements, added model serializers

---
+++ blogs/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from blogs.models import Blog
+
+
+class BlogSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
